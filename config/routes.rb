0b65e17871488fd2583ea4f62f4985edaecc2e46
steve-alex <steve.alex@hotmail.co.uk>
2019-11-04 15:04:46 +0000

    created edit form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index', to: 'home#index'
 
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   
   root :to => "home#index"
 end
