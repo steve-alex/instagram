b8bd2c49c3fe23801b04d68e5e6ef5be52ee01b6
steve-alex <steve.alex@hotmail.co.uk>
2019-11-05 15:44:08 +0000

    created home photo feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @posts = Post.all.where(user_id: current_user.id).order(created_at: :desc)
+    
+    # order(created_at: :desc)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,14 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   validate :image_presence
+  # validate :description length
 
   def image_presence
     errors.add(:image, "can't be blank") unless image.attached?
+  end
+
+  def owner
+    User.find(self.user_id).username
   end
 
 end
