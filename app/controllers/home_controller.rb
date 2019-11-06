bc489a931cfb4f14724deb8665b71a9edb7bf1e6
steve-alex <steve.alex@hotmail.co.uk>
2019-11-06 11:26:11 +0000

    created user show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all.where(user_id: current_user.id).order(created_at: :desc)
-    
-    # order(created_at: :desc)
   end
 end
