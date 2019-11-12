2042daeb2a0f0e628f72cc5e946956ed049ce5ca
steve-alex <steve.alex@hotmail.co.uk>
2019-11-12 10:46:12 +0000

    refactored main.js

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+    BASE_URL = `http://localhost:3000`
 
     const renderPage = () => {
         let postFeed = document.getElementsByClassName("posts")[0]
@@ -10,109 +11,84 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    const renderPostFeed = () => {
-        getUsersPostFeed()
-        .then(postData => postData.forEach(post => renderPost(post)))
-    }
-
-    let getUsersPostFeed = async () => {
-        let response = await fetch(`http://localhost:3000/users/1/feed`);
+    let renderPostFeed = async () => {
+        let response = await fetch(`${BASE_URL}/users/1/feed`);
         let results = await response.json()
-        let posts = []
         for (let i=0; i<results.length; i++){
-            let res = await fetch(`http://localhost:3000/posts/${results[i].id}`);
-            let secondRes = await res.json();
-            posts.push(secondRes)
+            let postResponse = await fetch(`${BASE_URL}/posts/${results[i].id}`);
+            let postResults = await postResponse.json();
+            renderPost(postResults)
         }
-        return posts
     }
     //https://medium.com/@imorobebh/javascript-fetch-synchronous-or-asynchronous-fd24f8ba6129
 
     const renderPost = (postData) => {
-        
         let posts = document.getElementsByClassName("posts")[0]
-        let postContainer = document.createElement("div")
-        postContainer.className = "post"
-        posts.appendChild(postContainer)
-
-        let postBanner = document.createElement("div")
-        postBanner.className = "post-banner"
-        postContainer.appendChild(postBanner)
-
-        let userAvatarContainer = document.createElement("div")
-        userAvatarContainer.className = "user-avatar"
-        postBanner.appendChild(userAvatarContainer)
-    
-        let avatar = document.createElement("img")
-        avatar.setAttribute("src", postData.user.avatar)
-        userAvatarContainer.appendChild(avatar)
-
-        let usernameContainer = document.createElement("div")
-        usernameContainer.setAttribute("class", "post-username")
-        postBanner.appendChild(usernameContainer)
-
-        let username = document.createElement("p")
-        username.textContent = `${postData.user.username}`
-        usernameContainer.appendChild(username)
-
-        let imageContainer = document.createElement("div")
-        imageContainer.setAttribute("class", "post-image")
-        postContainer.appendChild(imageContainer)
-        
-        let image = document.createElement("img")
-        image.setAttribute("src", `${postData.post.image_url}`)
-        image.setAttribute("class", "main-image")
-        imageContainer.appendChild(image)
-
-        let postGlyphs = document.createElement("div")
-        postGlyphs.setAttribute("class", "post-glyphs")
-        postContainer.appendChild(postGlyphs)
-
-        let glyphsContainer = document.createElement("div")
-        glyphsContainer.setAttribute("class", "glyph-container")
-        postGlyphs.appendChild(glyphsContainer)
-
-        let heart = document.createElement("i")
-        heart.setAttribute("class", "fa fa-heart-o")
-        heart.addEventListener("click", () => toggleHeart())
-        glyphsContainer.appendChild(heart)
-
-        let comment = document.createElement("i")
-        comment.setAttribute("class", "fa fa-comment-o")
-        glyphsContainer.appendChild(comment)
-
-        let likesContainer = document.createElement("div")
-        likesContainer.setAttribute("class", "post-likes")
-        postContainer.appendChild(likesContainer)
-
-        let likesCount = document.createElement("p")
-        likesCount.textContent = `${postData.post.likes_count} likes`
-        likesContainer.appendChild(likesCount)
-
-        let descriptionContainer = document.createElement("div")
-        descriptionContainer.setAttribute("class", "post-description")
-        postContainer.appendChild(descriptionContainer)
-
-        let description = document.createElement("p")
+        let postContainer = createObject("div", "post", posts)
+        let postBanner = createObject("div", "post-banner", postContainer)
+        let userAvatarContainer = createObject("div", "user-avatar", postBanner)
+        let avatar = createImage(postData.user.avatar, "", userAvatarContainer)
+        let usernameContainer = createObject("div", "post-username", postBanner)
+        let username = createObject("p", "", usernameContainer, `${postData.user.username}`)
+        let imageContainer = createObject("div", "post-image", postContainer)
+        let image = createImage(`${postData.post.image_url}`, "main-image", imageContainer)
+        let postGlyphs = createObject("div", "post-glyphs", postContainer)
+        let glyphsContainer = createObject("div", "glyphs-container", postGlyphs)
+        let heart = createHeartObject(postData, glyphsContainer)
+        heart.addEventListener("click", () => handleToggleHeart(postData))
+        let comment = createObject("i", "fa fa-comment-o", glyphsContainer)
+        let likesContainer = createObject("div", "post-likes", postContainer)
+        let likes_count = createObject("div", "", likesContainer, `${postData.post.likes_count} likes`)
+        let descriptionContainer = createObject("div", "post-description", postContainer)
+        let description = createObject("p", "", descriptionContainer)
         description.innerHTML = `<strong>${postData.user.username}</strong> ${postData.post.description}`
-        descriptionContainer.appendChild(description)
     }
 
-    const toggleHeart = () => {
-        if (event.target.style.color == "red") {
-            event.target.className = "fa fa-heart-o"
-            event.target.style.color = ""
+    const createHeartObject = (postData, parentObject) => {
+        let userAlreadyLikes = postData.post.current_user_likes;
+        if (userAlreadyLikes) {
+            return createObject("i", "fa fa-heart", parentObject, "", "red")
         } else {
-            event.target.className = "fa fa-heart"
-            event.target.style.color = "red"
+            return createObject("i", "fa fa-heart-o", parentObject)
         }
     }
 
-    const createObject = (objectTag, classname, parentObject) => {
+    const createObject = (objectTag, classname, parentObject, text, color) => {
         let object = document.createElement(objectTag)
         object.setAttribute("class", classname)
+        object.textContent = text
+        object.style.color = color
         parentObject.appendChild(object)
         return object
+    }
+
+    const createImage = (src, classname, parentObject) => {
+        let image = document.createElement("img");
+        image.setAttribute("src", src);
+        image.setAttribute("class", classname);
+        parentObject.appendChild(image);
+        return image
+    }
+
+    const handleToggleHeart = (postData) => {
+        //this could be based on whether user already likes the post?
+        if (event.target.style.color == "red") {
+            event.target.className = "fa fa-heart-o"
+            event.target.style.color = ""
+            unlikePost(postData)
+        } else {
+            event.target.className = "fa fa-heart"
+            event.target.style.color = "red"
+            likePost(postData)
+        }
+    }
+
+    const likePost = (postData) => {
+        return fetch('')
+    }
+
+    const unlikePost = (postData) => {
+
     }
 
     renderPage();
